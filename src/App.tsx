d1b17810259e94da41ab606f8ff22ab015d0c9b5
Sc0tticus <ssinger303@gmail.com>
2022-10-29 13:42:46 -0600

    Adding styled componet AddItemButton to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { AddNewItem } from './AddNewItem'
 import { Column } from './Column'
-import {
-	AppContainer
-} from "./styles"
+import { AppContainer } from "./styles"
 
 export const App = () => {
 	return (
 		<AppContainer>
 			<Column text="Todo:" />
+			<AddNewItem
+				toggleButtonText='+ Add another list'
+				onAdd={() => console.log('Item created')}
+			/>
 		</AppContainer>
 	)
 }
